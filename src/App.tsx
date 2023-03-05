589e11b954d2f06341e044c3c6519d653e0d7b8f
DumontGontran <dumont.gontran@orange.fr>
2023-03-05 03:55:19 +0100

    set main files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import TodoForm from './components/todoForm/TodoForm';
+import TodoList from './components/todoList/TodoList';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
