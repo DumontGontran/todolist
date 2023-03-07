2c92131345415e85542db9f06e841183d689f2aa
DumontGontran <dumont.gontran@orange.fr>
2023-03-07 02:56:35 +0100

    add conditional deletion onClick todoDelete function in Redux store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,10 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 interface Infos {
-    todos: Object[]
+    todos: any
     todo: {
+        id: number,
+        isDone: boolean,
         title: string,
         description: string,
         dueDate: string
@@ -14,11 +16,18 @@ interface SetInfos {
     payload: string
 }
 
+interface EventInfos {
+    type: string,
+    payload: number
+}
+
 const todoSlice = createSlice({
     name: 'Todo',
     initialState: {
         todos: [],
         todo: {
+            id: -1,
+            isDone: false,
             title: '',
             description: '',
             dueDate: new Date().toDateString()
@@ -35,11 +44,31 @@ const todoSlice = createSlice({
         },
         setDueDate: (state: Infos, action: SetInfos): void => {
             console.log(action.payload)
-            state.todo.dueDate = action.payload
+            state.todo.dueDate = action.payload.split('-').reverse().join('/')
         },
-        sendTodo: (state: Infos): any => {
-            state.todos = [...state.todos, state.todo]
+        sendTodo: (state: Infos): void => {
+            state.todo.id++
+            state.todos = [...state.todos, (state.todo)]
             console.log(state.todos)
+        },
+        todoDone: (state: Infos, action: EventInfos): void => {
+            const id: number = state.todos.findIndex((todo: { id: number }) => (todo.id) === action.payload)
+            console.log('index of todo button: ', id)
+
+            if (id === action.payload) {
+                state.todos[action.payload].isDone = true
+                console.log(state.todos[action.payload].isDone)
+            }
+        },
+        todoDelete: (state: Infos, action: EventInfos): void => {
+            if (state.todos[action.payload].isDone === false) {
+                let confirm: boolean = window.confirm("Do you really want to delete this task ?")
+                if (confirm === true) {
+                    state.todos = [...state.todos.filter((todo: { id: number }) => todo.id !== action.payload)]
+                }
+            } else {
+                state.todos = [...state.todos.filter((todo: { id: number }) => todo.id !== action.payload)]
+            }
         }
     }
 })
